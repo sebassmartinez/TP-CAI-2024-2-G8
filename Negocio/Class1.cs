7becc7661ab021243ae84a139760963ff6dc59d4
sebassmartinez <143354364+sebassmartinez@users.noreply.github.com>
2024-09-15 20:52:21 -0300

    userclass

--- Negocio/Class1.cs
+++ Negocio/Class1.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Negocio
-{
-    public class Class1
-    {
-    }
+    namespace Negocio
+    
+        public class LoginNegocio
+        {
+            public void Login(string usuario, string password)
+            {
+                ValidacionesUtiles validaciones = new ValidacionesUtiles();
+                if (validaciones.validarNombreUsuario(usuario, password))
+
+                { 
+
+                }
+            }
+        }
+    
 }
+
